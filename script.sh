66449946af3a596e32f17719ceda059d5590cfb4
Christian Paul <info@jaller.de>
2017-01-19 00:30:53 +0100

    First good version

--- script.sh
+++ script.sh
@@ -1,14 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 # Author: Christian Paul
 
 # Find all files in a given folder
 # Only process the first 3 files
 # Reduce relative files paths to their file names
 # Substring to the characters 3 to 6
-# Sort the file name parts ascending and 
-find "$1" -maxdepth 1 -type f | \
-  head -3 | \
-  awk -F "/" '{print $NF;}' | \
-  awk '{print substr($0,4,3);}' | \
-  sort -f
+# Sort the file name parts ascending
 
+DIR=${1:-./} #Defaults to ./ dir
+
+FILES="$(find "$DIR" -maxdepth 1 -type f | head -n 3)"
+
+sortcharacters() {
+	echo "$1" | grep -o . | sort | tr -d "\n"
+}
+
+while read -r FPATH; do
+	FNAME="$(basename -- "$FPATH")"
+	FNAME3="${FNAME:3:3}"
+	FNAME3SORTED="$(sortcharacters "$FNAME3")"
+	LINES="$(wc -l "$FPATH" | cut -d' ' -f1)"
+	echo "$FNAME3SORTED   $LINES"
+done <<< "$FILES"
